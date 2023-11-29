4f350c240e2a28b8d7ff81adf867f19d177f06bf
Raikiri45 <149266085+Raikiri45@users.noreply.github.com>
2023-11-29 17:57:24 +0000

    created method that explains the coordinate choosing system and part of a method that allows the players to choose coordinates. Also created a 2 dimensional array to hold player coordinates

--- BattleShips/Program.cs
+++ BattleShips/Program.cs
@@ -8,7 +8,7 @@ void Introduction()
     Console.WriteLine("one space at a time!");
 }
 //Create 10x10 grid
-void CreateBoard()
+void PrintInitialBoard()
 {
     int[,] board = new int[11, 11];
     for (int i = 0; i < 11; i++)
@@ -37,8 +37,35 @@ void CreateBoard()
         Console.WriteLine();
     }
 }
-CreateBoard();
 //Create 2 1 for each player
+void ExplainCoordinates()
+{
+    Console.WriteLine();
+    Console.WriteLine("Choose 3 sets of coordinates from the board");
+    Console.WriteLine("starting with it's X coordinate 1-10 (horizonatl)");
+    Console.WriteLine("then it's Y coordinate 1-10 (vertical)");
+}
+int[,] playerOneShipCoordinates = new int[3, 2];
+void PlayerShipCoordinates(int whoseTurnCounter)
+{
+    int whichPlayer = 0;
+    int howManyCoordinatesCounter = 1;
+    if (whoseTurnCounter % 2 == 0)
+    {
+        whichPlayer = 2;
+    }
+    else
+    {
+        whichPlayer = 1;
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Player {whichPlayer} choose X coordinate {howManyCoordinatesCounter}");
+    int xCoordinate = int.Parse(Console.ReadLine());
+    playerOneShipCoordinates[howManyCoordinatesCounter, 1] = xCoordinate;
+    Console.WriteLine();
+}
+
 //Create method for initialising a battle ship with a length of 3 for each player
+
 //Method for selecting position
 //method for comparing selected positiion and the position wiht ship
